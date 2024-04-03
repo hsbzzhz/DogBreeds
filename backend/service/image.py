fc859dee85452bb330e10250051a39af163fa0ad
Hogan Zhang <hsbzzhz@126.com>
2024-04-03 17:36:04 +0800

    Update image.py

--- backend/service/image.py
+++ backend/service/image.py
@@ -7,11 +7,8 @@ class ImageService:
     def get_image_by_num(num: int):
         url = "https://dog.ceo/api/breeds/image/random/{}".format(num)
 
-        response = requests.get(url, timeout=3)
-
-        data = response.json()
-        return data
-
-
-if __name__ == '__main__':
-    ImageService.get_image_by_num(8)
+        try:
+            response = requests.get(url, timeout=1.2)
+            return response.json()
+        except Exception as e:
+            return {'status': 500, 'message': str(e)}
