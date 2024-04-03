95f9e5620a4c5d8c37561a6712d741d607c964dc
Hogan Zhang <hsbzzhz@126.com>
2024-04-03 17:34:54 +0800

    Update images.py

--- backend/controller/images.py
+++ backend/controller/images.py
@@ -1,29 +1,33 @@
-import json
 
-from flask import Blueprint, request, current_app
+from flask import Blueprint, request, jsonify
 
+from entity.execution import Execution
 from service.image import ImageService
+from util.validator import Validator
 
 images_bp = Blueprint('images', __name__, url_prefix='/api')
 
 # 用于保存执行记录信息的列表
 execution_history = []
 
+
 @images_bp.route('/get-images', methods=['GET'])
 def get_images_info():
     args = request.args
     num = args.get('number', None)
-    start_time = args.get('start_time', '')
+    start_time = args.get('startTime', None)
 
-    # 对num进行校验
-
-    res = ImageService.get_image_by_num(int(num))
-    return_dict = {'return_code': '200', 'result': None}
-    return res
+    # 参数校验
+    if Validator.image_input_validator(start_time, num):
+        res = ImageService.get_image_by_num(int(num))
+    else:
+        res = {'status': 400, 'message': "Invalid input parameters!"}
+    res['execution_time'] = start_time
+    res['input'] = num
+    execution_history.append(Execution(**res))
+    return jsonify(res)
 
 
 @images_bp.route('/get-records', methods=['GET'])
 def get_records_info():
-    params = request.get_json()
-
-    return 20
+    return jsonify({'status': 200, 'message': execution_history[::-1]})
